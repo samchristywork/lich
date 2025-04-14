ee99049d51292ca478ba83e8c9a8b286c336a223
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-13 19:33:07 -0500

    feat: Implement display traits for Environment and Token datatypes
    This commit implementes traits for `Environment` and `Token` so that it
    is easier to print the data to `stdout` or convert it to a string.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const NORMAL: &str = "\x1b[0m";
 #[derive(Debug, Clone, PartialEq, Hash, Eq)]
 enum Node {
     Symbol(String),
-    Number(i32),
+    Number(i64),
     String(String),
     Bool(bool),
     List(Vec<Node>),
@@ -41,20 +41,45 @@ impl fmt::Display for Node {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 struct Environment {
     parent: Option<Box<Environment>>,
     variables: std::collections::HashMap<Node, Node>,
 }
 
-#[derive(Debug)]
+impl fmt::Display for Environment {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut result = String::new();
+        for (key, value) in &self.variables {
+            result.push_str(&format!("{key} => {value}\n"));
+        }
+        if let Some(parent) = &self.parent {
+            result.push_str(&parent.to_string());
+        }
+        write!(f, "{}", result)
+    }
+}
+
 enum Token {
     Symbol(String),
-    Number(i32),
+    Number(i64),
     String(String),
     Bool(bool),
     LParen,
     RParen,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Symbol(s) => write!(f, "Symbol({s})"),
+            Token::Number(n) => write!(f, "Number({n})"),
+            Token::String(s) => write!(f, "String({s})"),
+            Token::Bool(b) => write!(f, "Bool({b})"),
+            Token::LParen => write!(f, "("),
+            Token::RParen => write!(f, ")"),
+        }
+    }
 }
 
 fn main() {
