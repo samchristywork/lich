fc71762c0f3442743b5cb4b1445ced533e14a2c4
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-13 19:21:11 -0500

    feat: Add display implementation for the new Node enum
    `Node`s are represented as `enums` now instead of `struct`s. This may
    change if I find out we need extra data for debug purposes.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 const GREY: &str = "\x1b[90m";
 const NORMAL: &str = "\x1b[0m";
 
@@ -8,6 +10,35 @@ enum Node {
     String(String),
     Bool(bool),
     List(Vec<Node>),
+}
+
+impl fmt::Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let res = match self {
+            Node::Number(n) => n.to_string(),
+            Node::Bool(b) => b.to_string(),
+            Node::String(s) | Node::Symbol(s) => s.clone(),
+            Node::List(nodes) => {
+                let mut result = String::new();
+                result.push('(');
+                result.push_str(
+                    &nodes
+                    .iter()
+                    .map(Node::to_string)
+                    .collect::<Vec<_>>()
+                    .join(" "),
+                );
+                result.push(')');
+                result
+            }
+        };
+
+        if res.is_empty() {
+            write!(f, "nil")
+        } else {
+            write!(f, "{res}")
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
