f230d967e3e70b2d41e2edd1b0423f5b3dddff2e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-18 18:43:39 -0500

    feat: Add better error handling to arithmetic module
    This modulenow uses the `invalid_arguments` macro to print out a much
    better error message when things go wrong.

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -1,153 +1,93 @@
 use crate::environment::Environment;
+use crate::invalid_arguments;
 use crate::node::Node;
 
-//- (test "+" (+ 1 2 3) 6)
-//- (test "+" (+ -10 2 3) -5)
-//- (test "+" (+ 1 2 3 4) 10)
-//- (test "+" (+ 1 (+ 2 3)) 6)
+//- (test "+" (+ 1 2) 3)
+//- (test "+" (+ 0 0) 0)
+//- (test "+" (+ -1 1) 0)
 pub fn fn_add(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    let mut result = 0;
-    for n in arguments {
-        if let Node::Number(num) = n {
-            result += num;
-        } else {
-            return Err(format!("Invalid argument for addition {n:?}"));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Number(a + b)),
+        _ => invalid_arguments!("+", arguments, ["[Number(a), Number(b)]"]),
     }
-
-    Ok(Node::Number(result))
 }
 
 //- (test "-" (- 1 2) -1)
-//- (test "-" (- 1) -1)
-//- (test "-" (- 1 2 3) -4)
+//- (test "-" (- 0 0) 0)
+//- (test "-" (- -1 1) -2)
 pub fn fn_sub(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    match arguments.len().cmp(&1) {
-        std::cmp::Ordering::Equal => {
-            if let Node::Number(num) = &arguments[0] {
-                Ok(Node::Number(-num))
-            } else {
-                Err(format!(
-                    "Invalid argument for subtraction {:?}",
-                    arguments[0]
-                ))
-            }
-        }
-        std::cmp::Ordering::Greater => {
-            if let Node::Number(first) = &arguments[0] {
-                let rest = &arguments[1..];
-                let mut result = *first;
-                for n in rest {
-                    if let Node::Number(num) = n {
-                        result -= num;
-                    } else {
-                        return Err(format!("Invalid argument for subtraction {n:?}"));
-                    }
-                }
-                Ok(Node::Number(result))
-            } else {
-                Err(format!(
-                    "Invalid argument for subtraction {:?}",
-                    arguments[0]
-                ))
-            }
-        }
-        std::cmp::Ordering::Less => {
-            Err(format!("Invalid arguments for subtraction: {arguments:?}"))
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Number(a - b)),
+        _ => invalid_arguments!("-", arguments, ["[Number(a), Number(b)]"]),
     }
 }
 
 //- (test "*" (* 1 2) 2)
-//- (test "*" (* 1) 1)
-//- (test "*" (* 1 2 -3) -6)
+//- (test "*" (* 0 0) 0)
+//- (test "*" (* -1 1) -1)
 pub fn fn_mult(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    let mut result = 1;
-    for n in arguments {
-        if let Node::Number(num) = n {
-            result *= num;
-        } else {
-            return Err(format!("Invalid argument for multiplication {n:?}"));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Number(a * b)),
+        _ => invalid_arguments!("*", arguments, ["[Number(a), Number(b)]"]),
     }
-
-    Ok(Node::Number(result))
 }
 
 //- (test "even?" (even? 2) true)
 //- (test "even?" (even? 3) false)
 //- (test "even?" (even? 0) true)
 pub fn fn_is_even(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Bool(num % 2 == 0));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Bool(num % 2 == 0)),
+        _ => invalid_arguments!("even?", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for even?: {arguments:?}"))
 }
 
 //- (test "odd?" (odd? 2) false)
 //- (test "odd?" (odd? 3) true)
 //- (test "odd?" (odd? 0) false)
 pub fn fn_is_odd(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Bool(num % 2 != 0));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Bool(num % 2 != 0)),
+        _ => invalid_arguments!("odd?", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for odd?: {arguments:?}"))
 }
 
 //- (test "inc" (inc 1) 2)
 //- (test "inc" (inc 0) 1)
 //- (test "inc" (inc -1) 0)
 pub fn fn_inc(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Number(num + 1));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Number(num + 1)),
+        _ => invalid_arguments!("inc", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for inc: {arguments:?}"))
 }
 
 //- (test "dec" (dec 1) 0)
 //- (test "dec" (dec 0) -1)
 //- (test "dec" (dec -1) -2)
 pub fn fn_dec(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Number(num - 1));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Number(num - 1)),
+        _ => invalid_arguments!("dec", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for dec: {arguments:?}"))
 }
 
 //- (test "abs" (abs 1) 1)
 //- (test "abs" (abs -1) 1)
 //- (test "abs" (abs 0) 0)
 pub fn fn_abs(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Number(num.abs()));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Number(num.abs())),
+        _ => invalid_arguments!("abs", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for abs: {arguments:?}"))
 }
 
 //- (test "negate" (negate 1) -1)
 //- (test "negate" (negate -1) 1)
 //- (test "negate" (negate 0) 0)
 pub fn fn_negate(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Number(num) = &arguments[0] {
-            return Ok(Node::Number(-num));
-        }
+    match arguments {
+        [Node::Number(num)] => Ok(Node::Number(-num)),
+        _ => invalid_arguments!("negate", arguments, ["[Number(num)]"]),
     }
-
-    Err(format!("Invalid arguments for negate: {arguments:?}"))
 }
--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,11 @@
-#[macro_export] macro_rules! invalid_arguments {
+#[macro_export]
+macro_rules! invalid_arguments {
     ($name:expr, $arguments:expr, $expected:expr) => {
         Err(format!(
             "Invalid arguments for '{}'.\n\nFound: {:?}\n\nExpected one of:\n{}",
-            $name, $arguments, $expected
+            $name,
+            $arguments,
+            $expected
                 .iter()
                 .map(|s| format!("- {}", s))
                 .collect::<Vec<_>>()
