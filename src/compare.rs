71e129569a3684a18c9909e54c457caeb21d4b5b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-18 18:44:49 -0500

    feat: Add better error handling to compare module
    The Compare module now has much nicer error messages thanks to the macro
    in the Error module.

--- src/compare.rs
+++ src/compare.rs
@@ -1,114 +1,98 @@
 use crate::environment::Environment;
+use crate::invalid_arguments;
 use crate::node::Node;
 
 //- (test "=" (= 1 2) false)
 //- (test "=" (= "foo" "bar") false)
 //- (test "=" (= 1 1) true)
 pub fn fn_eq(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        return Ok(Node::Bool(arguments[0] == arguments[1]));
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Bool(a == b)),
+        [Node::Text(a), Node::Text(b)] => Ok(Node::Bool(a == b)),
+        [Node::List(a), Node::List(b)] => Ok(Node::Bool(a == b)),
+        [Node::Bool(a), Node::Bool(b)] => Ok(Node::Bool(a == b)),
+        [Node::Symbol(a), Node::Symbol(b)] => Ok(Node::Bool(a == b)),
+        [Node::Time(t1, z1), Node::Time(t2, z2)] => Ok(Node::Bool(t1 == t2 && z1 == z2)),
+        _ => invalid_arguments!(
+            "=",
+            arguments,
+            [
+                "[Number(a), Number(b)]",
+                "[Text(a), Text(b)]",
+                "[List(a), List(b)]",
+                "[Bool(a), Bool(b)]",
+                "[Symbol(a), Symbol(b)]",
+                "[Time(t1, z1), Time(t2, z2)]"
+            ]
+        ),
     }
-    Err(format!(
-        "Invalid arguments for equality check: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (test "<" (< 1 2) true)
 //- (test "<" (< 2 1) false)
 //- (test "<" (< 1 1) false)
 pub fn fn_less_than(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let (Node::Number(a), Node::Number(b)) = (&arguments[0], &arguments[1]) {
-            return Ok(Node::Bool(a < b));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Bool(a < b)),
+        _ => invalid_arguments!("<", arguments, ["[Number(a), Number(b)]"]),
     }
-    Err(format!(
-        "Invalid arguments for less than check: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (test ">" (> 1 2) false)
 //- (test ">" (> 2 1) true)
 //- (test ">" (> 1 1) false)
 pub fn fn_greater_than(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let (Node::Number(a), Node::Number(b)) = (&arguments[0], &arguments[1]) {
-            return Ok(Node::Bool(a > b));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Bool(a > b)),
+        _ => invalid_arguments!(">", arguments, ["[Number(a), Number(b)]"]),
     }
-    Err(format!(
-        "Invalid arguments for greater than check: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (test "<=" (<= 1 2) true)
 //- (test "<=" (<= 2 1) false)
 //- (test "<=" (<= 1 1) true)
 pub fn fn_less_than_or_equal(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let (Node::Number(a), Node::Number(b)) = (&arguments[0], &arguments[1]) {
-            return Ok(Node::Bool(a <= b));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Bool(a <= b)),
+        _ => invalid_arguments!("<=", arguments, ["[Number(a), Number(b)]"]),
     }
-    Err(format!(
-        "Invalid arguments for less than or equal check: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (test ">=" (>= 1 2) false)
 //- (test ">=" (>= 2 1) true)
 //- (test ">=" (>= 1 1) true)
 pub fn fn_greater_than_or_equal(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let (Node::Number(a), Node::Number(b)) = (&arguments[0], &arguments[1]) {
-            return Ok(Node::Bool(a >= b));
-        }
+    match arguments {
+        [Node::Number(a), Node::Number(b)] => Ok(Node::Bool(a >= b)),
+        _ => invalid_arguments!(">=", arguments, ["[Number(a), Number(b)]"]),
     }
-    Err(format!(
-        "Invalid arguments for greater than or equal check: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (test "not" (not true) false)
 //- (test "not" (not false) true)
 pub fn fn_not(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 1 {
-        if let Node::Bool(b) = &arguments[0] {
-            return Ok(Node::Bool(!b));
-        }
+    match arguments {
+        [Node::Bool(a)] => Ok(Node::Bool(!a)),
+        _ => invalid_arguments!("not", arguments, ["[Bool(a)]"]),
     }
-    Err(format!("Invalid arguments for not: {:?}", &arguments[0]))
 }
 
 //- (test "and" (and true true) true)
 //- (test "and" (and true false) false)
 //- (test "and" (and false true) false)
 pub fn fn_and(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let Node::Bool(a) = &arguments[0] {
-            if let Node::Bool(b) = &arguments[1] {
-                return Ok(Node::Bool(*a && *b));
-            }
-        }
+    match arguments {
+        [Node::Bool(a), Node::Bool(b)] => Ok(Node::Bool(*a && *b)),
+        _ => invalid_arguments!("and", arguments, ["[Bool(a), Bool(b)]"]),
     }
-    Err(format!("Invalid arguments for and: {:?}", &arguments[0]))
 }
 
 //- (test "or" (or true true) true)
 //- (test "or" (or true false) true)
 //- (test "or" (or false false) false)
 pub fn fn_or(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let Node::Bool(a) = &arguments[0] {
-            if let Node::Bool(b) = &arguments[1] {
-                return Ok(Node::Bool(*a || *b));
-            }
-        }
+    match arguments {
+        [Node::Bool(a), Node::Bool(b)] => Ok(Node::Bool(*a || *b)),
+        _ => invalid_arguments!("or", arguments, ["[Bool(a), Bool(b)]"]),
     }
-    Err(format!("Invalid arguments for or: {:?}", &arguments[0]))
 }
