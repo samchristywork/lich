b96b2f09bd83617e03b29ac1b4955f9cd1855995
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-19 17:35:48 -0500

    feat: Add better error messages to sequence module
    This module now has better error messages through the
    `invalid_arguments` macro, which returns a formatted error message.

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,86 +1,98 @@
 use crate::environment::Environment;
 use crate::eval::apply;
+use crate::invalid_arguments;
 use crate::node::Node;
 
 //- (test "fold" (fold + 0 (quote (1 2 3))) 6)
 //- (test "fold" (fold + 0 (quote ())) 0)
 //- (test "fold" (fold + 0 (quote (1))) 1)
 pub fn fn_fold(arguments: &[Node], env: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 3 {
-        let function = &arguments[0];
-        let initial_value = &arguments[1];
-        let list = &arguments[2];
-
-        if let Node::List(l) = list {
+    match arguments {
+        [function, initial_value, Node::List(list)] => {
             let mut result = initial_value.clone();
-            for item in l {
+            for item in list {
                 result = apply(function, &[result, item.clone()], env)?;
             }
-            return Ok(result);
+            Ok(result)
         }
+        _ => invalid_arguments!(
+            "fold",
+            arguments,
+            ["[Any(function), Any(initial_value), List(list)]"]
+        ),
     }
-    Err(format!("Invalid arguments for fold: {:?}", &arguments[0]))
 }
 
 //- (test "zip" (zip (quote (1 2 3)) (quote (4 5 6))) (quote ((1 4) (2 5) (3 6))))
 //- (test "zip" (zip (quote ()) (quote ())) (quote ()))
 //- (test "zip" (zip (quote (1)) (quote (2))) (quote ((1 2))))
 pub fn fn_zip(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        let list1 = &arguments[0];
-        let list2 = &arguments[1];
-
-        if let (Node::List(l1), Node::List(l2)) = (list1, list2) {
+    match arguments {
+        [Node::List(list1), Node::List(list2)] => {
             let mut zipped = Vec::new();
-            for (item1, item2) in l1.iter().zip(l2.iter()) {
-                zipped.push(Node::List(vec![item1.clone(), item2.clone()]));
+            let len = list1.len().min(list2.len());
+            for i in 0..len {
+                zipped.push(Node::List(vec![list1[i].clone(), list2[i].clone()]));
             }
-            return Ok(Node::List(zipped));
+            Ok(Node::List(zipped))
         }
+        _ => invalid_arguments!("zip", arguments, ["[List(list1), List(list2)]"]),
     }
-    Err(format!("Invalid arguments for zip: {:?}", &arguments[0]))
 }
 
 //- (test "range" (range 1 5) (quote (1 2 3 4)))
 //- (test "range" (range 5) (quote (0 1 2 3 4)))
 //- (test "range" (range 0 0) (quote ()))
 pub fn fn_range(arguments: &[Node], _: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        if let (Node::Number(start), Node::Number(end)) = (&arguments[0], &arguments[1]) {
-            let mut range = Vec::new();
-            for i in *start..*end {
-                range.push(Node::Number(i));
-            }
-            return Ok(Node::List(range));
-        }
-    } else if arguments.len() == 1 {
-        if let Node::Number(end) = &arguments[0] {
+    match arguments {
+        [Node::Number(end)] => {
             let mut range = Vec::new();
             for i in 0..*end {
                 range.push(Node::Number(i));
             }
-            return Ok(Node::List(range));
+            Ok(Node::List(range))
         }
+        [Node::Number(start), Node::Number(end)] => {
+            let mut range = Vec::new();
+            for i in *start..*end {
+                range.push(Node::Number(i));
+            }
+            Ok(Node::List(range))
+        }
+        [Node::Number(start), Node::Number(end), Node::Number(step)] => {
+            let mut range = Vec::new();
+            if *step == 0 {
+                return Err("Step cannot be zero".to_string());
+            }
+            let mut current = *start;
+            while (step > &0 && current < *end) || (step < &0 && current > *end) {
+                range.push(Node::Number(current));
+                current += step;
+            }
+            Ok(Node::List(range))
+        }
+        _ => invalid_arguments!(
+            "range",
+            arguments,
+            [
+                "[Number(end)",
+                "Number(start), Number(end)",
+                "Number(start), Number(end), Number(step)]"
+            ]
+        ),
     }
-    Err(format!("Invalid arguments for range: {:?}", &arguments[0]))
 }
 
 pub fn fn_for_each(arguments: &[Node], env: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        let function = &arguments[0];
-        let list = &arguments[1];
-
-        if let Node::List(l) = list {
-            for item in l {
+    match arguments {
+        [function, Node::List(list)] => {
+            for item in list {
                 apply(function, &[item.clone()], env)?;
             }
-            return Ok(Node::Bool(true));
+            Ok(Node::Bool(true))
         }
+        _ => invalid_arguments!("for-each", arguments, ["[Any(function), List(list)]"]),
     }
-    Err(format!(
-        "Invalid arguments for for-each: {:?}",
-        &arguments[0]
-    ))
 }
 
 //- (define inc (lambda (x) (+ x 1)))
@@ -88,42 +100,32 @@ pub fn fn_for_each(arguments: &[Node], env: &mut Environment) -> Result<Node, St
 //- (test "map" (map inc (quote ())) (quote ()))
 //- (test "map" (map inc (quote (1))) (quote (2)))
 pub fn fn_map(arguments: &[Node], env: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        let function = &arguments[0];
-        let list = &arguments[1];
-
-        if let Node::List(l) = list {
+    match arguments {
+        [function, Node::List(list)] => {
             let mut mapped = Vec::new();
-            for item in l {
+            for item in list {
                 mapped.push(apply(function, &[item.clone()], env)?);
             }
-            return Ok(Node::List(mapped));
+            Ok(Node::List(mapped))
         }
+        _ => invalid_arguments!("map", arguments, ["[Any(function), List(list)]"]),
     }
-    Err(format!("Invalid arguments for map: {:?}", &arguments[0]))
 }
 
 //- (test "filter" (filter even? (quote (1 2 3 4))) (quote (2 4)))
 //- (test "filter" (filter even? (quote ())) (quote ()))
 //- (test "filter" (filter even? (quote (1))) (quote ()))
 pub fn fn_filter(arguments: &[Node], env: &mut Environment) -> Result<Node, String> {
-    if arguments.len() == 2 {
-        let function = &arguments[0];
-        let list = &arguments[1];
-
-        if let Node::List(l) = list {
+    match arguments {
+        [function, Node::List(list)] => {
             let mut filtered = Vec::new();
-            for item in l {
-                if let Node::Bool(b) = apply(function, &[item.clone()], env)? {
-                    if b {
-                        filtered.push(item.clone());
-                    }
-                } else {
-                    return Err(format!("Function did not return a boolean value: {item:?}",));
+            for item in list {
+                if let Node::Bool(true) = apply(function, &[item.clone()], env)? {
+                    filtered.push(item.clone());
                 }
             }
-            return Ok(Node::List(filtered));
+            Ok(Node::List(filtered))
         }
+        _ => invalid_arguments!("filter", arguments, ["[Any(function), List(list)]"]),
     }
-    Err(format!("Invalid arguments for filter: {:?}", &arguments[0]))
 }
