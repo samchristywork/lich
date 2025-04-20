a26fd9e05bc8e6ddc5f4257efc53a2a10ba05a4c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-20 18:53:48 -0500

    feat: Implement random numbers for Floats
    `random-number` will now generate Float values if the arguments passed
    to it are Floats.

--- src/random.rs
+++ src/random.rs
@@ -22,10 +22,30 @@ pub fn fn_random_number(arguments: &[Node]) -> Result<Node, String> {
             let random_number = rand::rng().random_range(min..=max);
             Ok(Node::Number(random_number))
         }
+        [Node::Float(max)] => {
+            if *max < 0.0 {
+                return Err("Maximum value cannot be negative".to_string());
+            }
+            let max = *max;
+            let random_number = rand::rng().random_range(0.0..=max);
+            Ok(Node::Float(random_number))
+        }
+        [Node::Float(min), Node::Float(max)] => {
+            if min > max {
+                return Err("Minimum value cannot be greater than maximum value".to_string());
+            }
+            let min = *min;
+            let max = *max;
+            let random_number = rand::rng().random_range(min..=max);
+            Ok(Node::Float(random_number))
+        }
         _ => invalid_arguments!(
             "random-number",
             arguments,
-            ["[]", "[Number(min), Number(max)]"]
+            [
+            "[]", "[Number(max)]", "[Number(min), Number(max)]",
+            "[]", "[Float(max)]", "[Float(min), Float(max)]"
+            ]
         ),
     }
 }
